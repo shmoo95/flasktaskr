2af8bb0d7d18a14969d1a93a2341662da3c86401
Joseph C. Burwasser <josephcburwasser@gmail.com>
2014-10-10 21:21:29 -0600

    blah

---
+++ app/config.py
@@ -0,0 +1,14 @@
+import os
+
+#grabs the folder where the script runs
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+DATABASE = 'flasktaskr.db'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+WTF_CSRF_ENABLED = True
+SECRET_KEY = 'my_precious'
+
+#defines the full path for the DATABASE
+DATABASE_PATH = os.path.join(basedir, DATABASE)
+
---
+++ app/views.py
@@ -0,0 +1,55 @@
+#views.py
+
+from flask import Flask, flash, redirect, render_template, request, session, url_for, g
+from functools import wraps
+import sqlite3
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+def connect_db():
+	return sqlite.connect(app.config['DATABASE'])
+
+def login_required(test):
+	@wraps(test)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return test(*args, **kwargs)
+		else:
+			flash('You need to login first.')
+			return redirect(url_for('login'))
+	return wrap
+
+@app.route('/logout/')
+def logout():
+	session.pop('logged_in', None)
+	flash('You are logged out. Bye. :(')
+	return redirect(url_for('login'))
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
+			error = 'Invalid Credentials. Please try again'
+			return render_template('login.html', error=error)
+		else:
+			session['logged_in'] = True
+			return redirect(url_for('tasks'))
+	if request.method == 'GET':
+		return render_template('login.html')
+
+@app.route('/tasks/')
+@login_required
+def tasks():
+	g.db = connect_db()
+	cur = g.db.execute('select name, due_date, priority, task_id from tasks where status=1')
+	open_tasks = [dict(name=row[0], due_date = row[1], priority = row[2], task_id = row[3]) for row in cur.fetchall()]
+	cur = g.db.execute('select name, due_date, priority, task_id from tasks where status=0')
+	closed_tasks = [dict(name=row[0], due_date=row[1], priority = row[2], task_id = row[3]) for row in cur.fetchall()]
+	g.db.close()
+	return render_template(
+		'tasks.html',
+		form=AddTaskForm(request.form),
+		open_tasks=open_tasks,
+		closed_tasks=closed_tasks
+		)
